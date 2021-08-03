b0e7ecfd77160c19ba860af699d57d335bcf80f6
Aiden Swann <swannaiden@gmail.com>
2021-08-03 11:58:09 -0700

    new versions

--- oldreadlogs.m
+++ oldreadlogs.m
@@ -1,43 +1,11 @@
-startBytes = uint8([49,50]);
-
-receiverNew = uint8(1);
-receiverData = int32([101,202,303,404,505,606,707,808]);
-receiverDataBytes = typecast(receiverData,'uint8');
-
-lidarNew = uint8(1);
-lidarData = int32(99999);
-lidarDataBytes = typecast(lidarData,'uint8');
-
-sonarNew = uint8(1);
-sonarData = int32(50000);
-sonarDataBytes = typecast(sonarData,'uint8');
-
-gpsNew = uint8(1);
-gpsData = double([9.09,10.10,11.11]);
-gpsDataBytes = typecast(gpsData,'uint8');
-numSatsBytes = uint8(5);
-
-imuNew = uint8(1);
-imuData = single([10.01,12.02,13.03,14.04,15.05,16.06,17.07,18.08,19.09,20.20]);
-imuDataBytes = typecast(imuData,'uint8');
-
-timestamp = uint32(1111111);
-timestampBytes = typecast(timestamp,'uint8');
-
-stopBytes = uint8([60,59]);
-
-dataMsg = [startBytes,receiverNew,receiverDataBytes,lidarNew, ...
-    lidarDataBytes,sonarNew,sonarDataBytes,gpsNew,gpsDataBytes, ...
-    numSatsBytes, imuNew, imuDataBytes, timestampBytes, stopBytes];
-dataMsg = [dataMsg,[1,2,3],dataMsg];
-%% read file
-fileID = fopen('BlackBox/rosebow2/log224.txt');
+%% read teensy log
+fileID = fopen('log234.txt');
 A = fread(fileID);
 dataMsg = uint8(A);
 
 clearvars -except dataMsg
 i = 1;
-msgLength = 122;
+msgLength = 142;
 idx = 1;
 receiver_idx = 1;
 lidar_idx = 1;
@@ -53,6 +21,7 @@ while (i < length(dataMsg))
             for j = 1:8
                 receiver(receiver_idx,j) = typecast(dataMsg(i+3+(j-1)*4:i+3+(j-1)*4+3),'int32');
             end
+            receiver_t(receiver_idx) =  typecast(dataMsg(i+116:i+119),'uint32');
             receiver_idx = receiver_idx+1;
         end
         
@@ -80,10 +49,18 @@ while (i < length(dataMsg))
             for j= 1:10
                 imu(imu_idx,j) = typecast(dataMsg(i+76+(j-1)*4:i+79+(j-1)*4),'single');
             end
+            for j = 1:3
+                mag(imu_idx,j) = typecast(dataMsg(i+120+(j-1)*4:i+123+(j-1)*4),'single');
+            end
+            temp(imu_idx) =  typecast(dataMsg(i+132:i+135),'single');
+            bar(imu_idx) =  typecast(dataMsg(i+136:i+139),'single');
+            
+            imu_t(imu_idx) =  typecast(dataMsg(i+116:i+119),'uint32');
             imu_idx = imu_idx+1;
         end
         
         timestamp(idx) = typecast(dataMsg(i+116:i+119),'uint32');
+        
         idx = idx + 1;
         
         i = i + msgLength;
@@ -93,29 +70,93 @@ while (i < length(dataMsg))
     end
 end
 
-receiver;
-lidar;
-sonar;
-gps;
-gps_sat_num;
-imu;
-timestamp;
-badByteCount;
 fprintf('Bad bytes: %i, IMU frequency: %f, lidar: %f, sonar: %f, gps: %f\n', ...
     badByteCount,imu_idx/(-(single(timestamp(1))-single(timestamp(end)))*1e-6), ...
     lidar_idx/(-(single(timestamp(1))-single(timestamp(end)))*1e-6), ...
     sonar_idx/(-(single(timestamp(1))-single(timestamp(end)))*1e-6), ...
     gps_idx/(-(single(timestamp(1))-single(timestamp(end)))*1e-6));
 
-plot(timestamp(2:end)-timestamp(1:end-1))
-
-%%
+gps_t_f = double(gps_t - timestamp(1))*1e-6;
+imu_t_f = double(imu_t - timestamp(1))*1e-6;
+receiver_t_f = double(receiver_t - timestamp(1))*1e-6;
 
 gps_f(:,1) = double(gps(1:1:end,1))*1e-7;
 gps_f(:,2) = double(gps(1:1:end,2))*1e-7;
+gps_f(:,3) = double(gps(1:1:end,3))*1e-3;
+gps_f(:,3) = gps_f(:,3)-gps_f(1,3);
+
 
 gps_v = double(gps(:,4:6))*1e-3;
 
+%% read black box
+Tbl = readtable('BlackBox/rosebow2/flight1rose.csv');
+VarNames = Tbl.Properties.VariableNames;
+bb_roll = Tbl.heading_0_;
+bb_pitch = Tbl.heading_1_;
+bb_yaw = Tbl.heading_2_;
+bb_rates = [Tbl.gyroADC_0_,Tbl.gyroADC_1_,Tbl.gyroADC_2_];
+bb_alt = Tbl.BaroAlt ./100;
+bb_t = double(Tbl.time)*1e-6;
+bb_t = bb_t-bb_t(1);
+
+%% convert imu data reference frames
+clearvars roll pitch yaw
+tmp = quatinv(imu(1,1:4));
+tmp2 = quaternion(tmp);
+
+for i = 1:length(imu)
+   tmp = quaternion(imu(i,1:4));
+   tmp3 = tmp2*tmp;
+   tmp4 = eulerd(tmp2*tmp,'ZYX','frame');
+   roll(i) = tmp4(1)*pi/180;
+   pitch(i) = tmp4(2)*pi/180;
+   yaw(i) = tmp4(3)*pi/180;
+end
+%% plot imu data
+close all
+figure(1);
+subplot(3,2,1)
+plot(imu_t_f,-roll)
+hold on
+plot(bb_t,bb_roll-bb_roll(1))
+hold off
+subplot(3,2,3)
+plot(imu_t_f,pitch)
+hold on
+plot(bb_t,bb_pitch-bb_pitch(1))
+hold off
+subplot(3,2,5)
+plot(imu_t_f,yaw)
+hold on
+plot(bb_t,bb_yaw-bb_yaw(1))
+hold off
+
+subplot(3,2,2)
+plot(imu_t_f,-imu(:,5))
+hold on
+plot(bb_t,-(1/(180/pi))*bb_rates(:,1))
+hold off
+subplot(3,2,4)
+plot(imu_t_f,imu(:,6))
+hold on
+plot(bb_t,-(1/(180/pi))*bb_rates(:,2))
+hold off
+subplot(3,2,6)
+plot(imu_t_f,imu(:,7))
+hold on
+plot(bb_t,-(1/(180/pi))*bb_rates(:,3))
+hold off
+
+sgtitle('VN-100 (blue) vs onboard IMU (orange)')
+
+%% altitude comparison
+figure(2)
+plot(gps_t_f,gps_f(:,3))
+hold on
+plot(bb_t,bb_alt)
+hold off
+title('gps height vs onboard altimeter');
+%%
 close all
 figure
 labels = ["x velocity", "y velocity", "z velocity"];
@@ -127,7 +168,8 @@ xlabel('t')
 ylabel('m/s')
 end
 subplot(4,1,4)
-plot(sqrt(gps_v(:,1).^2+gps_v(:,2).^2))
+plot(sqrt(gps_v(:,1).^2+gps_v(:,2).^2+gps_v(:,3).^2))
+
 title('magnitude')
 xlabel('t')
 ylabel('m/s')
@@ -158,6 +200,8 @@ plot(sqrt(gps_dx(:,1).^2+gps_dx(:,2).^2))
 
 
 %% 
+close all
+
 
 avgLat = mean(gps_f(:,1));
 avgLong = mean(gps_f(:,2));
@@ -168,6 +212,8 @@ gps_avg(:,2) = deg2rad(gps_f(:,2)- gps_f(1,2));
 
 x = 6.3781e6 .* gps_avg(:,2).* cos(avgLat./360.*2*pi);
 y = 6.3781e6 .* gps_avg(:,1);
+z = (double(gps(:, 3)-gps(1,3))./1e3)';
+
 figure(2)
 subplot(3,1,1)
 plot(x)
@@ -177,25 +223,95 @@ subplot(3,1,3);
 plot(sqrt(x.^2+y.^2));
 
 figure
-z = zeros(size(x'));
-time = (1:length(x))';
-%time = (sqrt(gps_v(:,1).^2+gps_v(:,2).^2)).*2.237;
+%z = zeros(size(x'));
+%time = (1:length(x))';
+time = (sqrt(gps_v(:,1).^2+gps_v(:,2).^2+gps_v(:,3).^2)).*2.237;
+
+
+patch([180 -1 -1 180], [180 180 -1 -1], [0 0 0 0], 'white')  
+
 surface([x';x'],[y';y'],[z;z],[time';time'],...
         'facecol','no',...
         'edgecol','interp',...
         'linew',2);
-    
-%  h = colorbar;
-%  ylabel = get(h,'YTickLabel');
-%  mm = repmat(' mph',size(ylabel,1),1);
-%  ylabel = [ylabel mm];
-%  set(h,'YTickLabel',ylabel);
+
+h = colorbar;
 xlabel("east (m)");
 ylabel("west (m)");
 title("flight 1 data");
 
+[roll, pitch, yaw] = quat2angle(quats(:, 1:4));
+X = cos(yaw);%.*cos(pitch);
+Y = sin(yaw);%.*cos(pitch);
+%Z = sin(pitch);
+Z = zeros(length(X),1);
+X = X(1:floor(length(X)/length(gps)):end);
+Y = Y(1:floor(length(X)/length(gps)):end);
+Z = Z(1:floor(length(X)/length(gps)):end);
+X = X(1:length(z));
+Y = Y(1:length(z));
+Z = Z(1:length(z));
 
- 
+quiver3(x,y,z',X,Y,Z);
+
+% figure
+% axis([0 180 0 180 0 35])
+% 
+% for i = 1:length(gps_t)
+%     plot3(x(1:i), y(1:i), z(1:i), 'b')
+%     drawnow limitrate
+% end
+
+%% Compare desired angle rates
+rcCommand = (double(receiver(:,2:4))-172)./((1811-172)./2) -1;
+rcCommand = rcCommand .*-1;
+d_rates = 200.*rcCommand.*(1./(-1+(abs(rcCommand).*.7)))./360.*2.*pi;
+
+figure
+p1 = subplot(3,1,1);
+plot(imu_t, imu(:,7))
+hold on
+plot(receiver_t, d_rates(:,3))
+
+p2 = subplot(3,1,2);
+plot(imu_t, imu(:,6))
+hold on
+plot(receiver_t, -d_rates(:,2))
+
+p3 = subplot(3,1,3);
+plot(imu_t, imu(:,5))
+hold on
+plot(receiver_t, d_rates(:,1))
+linkaxes([p1 p2 p3], 'x')
+
+%%
+clc
+% yaw(yaw < 0) = yaw(yaw < 0) + 2*pi;
+
+%pitch = zeros(1,length(yaw));
+%roll = zeros(1,length(yaw));
+clear quats_tmp;
+quats = imu(:,1:4);
+for i = 1:1%length(quats)
+    quat2eul(quats(i,1:4))
+    tmp = quat2rotm(quats(i,:));
+    tmp = rotx(180)*transpose(tmp);
+    quats_tmp(i,:) = rotm2quat(tmp);
+    quat2eul(quats_tmp(i,1:4))
+end
+%%
+quats_int = interp1(imu_t_f,imu(:,1:4),gps_t_f);
+
+figure
+trajectory3(x,y,z, quats_int , .5, 100, 'gripen')
+axis equal
+axis([0 180 0 180 0 35])
+
+
+%%
+
+
+
 %%
 lat = double(gps(:, 1:10:end))*1e-7;
 lon = double(gps(:,2:10:end))*1e-7;
@@ -208,24 +324,3 @@ geoshow(lat, lon, 'DisplayType', 'line')
 
 latlim = [latlim(1)-.05, latlim(2) + .05];
 lonlim = [lonlim(1)-.05, lonlim(2) + .05];
-
-%%
-
-%latlim = [37.78 37.84]; 
-%lonlim = [-122.53 -122.40];
-
-info = wmsinfo('http://basemap.nationalmap.gov/ArcGIS/services/USGSImageryOnly/MapServer/WMSServer?');
-layer = info.Layer(1);
-
-
-
-%[A, R] = wmsread(layer, 'Latlim', latlim, 'Lonlim', lonlim);
-[A,R] = wmsread(layer,'Latlim',latlim, ...
-                           'Lonlim',lonlim, ...
-                           'ImageHeight',1024, ...
-                           'ImageWidth',1024);
-figure
-usamap(A,R)
-geoshow(A,R)
-geoshow(lat, lon, 'DisplayType', 'line', 'Color', 'red', ...
-    'Marker', 'diamond', 'MarkerEdgeColor', 'blue');
---
+++ small_drone.ino
@@ -0,0 +1,786 @@
+
+#include <SD.h>
+#include <SPI.h>
+#include "SBUS.h"
+#include "TeensyThreads.h"
+
+const int chipSelect = BUILTIN_SDCARD;
+
+int speedtest;
+// SBUS setup
+bool isSbusAvailable = 0;
+uint16_t channels[16];
+bool failSafe;
+bool lostFrame;
+SBUS x8r(Serial2);
+bool armed = false;
+
+
+// Lidar Setup
+#include <Wire.h>
+#include <VL53L0X.h>
+
+VL53L0X sensor;
+bool isLidarAvailable = 0;
+int range;
+
+//Sonar setup
+int sonarPin = 14;
+int sonarValue;
+bool isSonarAvailable = 0;
+
+//GPS Setup
+#include "SparkFun_Ublox_Arduino_Library.h" //http://librarymanager/All#SparkFun_u-blox_GNSS
+SFE_UBLOX_GPS myGPS;
+#include <SoftwareSerial.h>
+long lastTime = 0; //Simple local timer. Limits amount of I2C traffic to Ublox module.
+long lastArmed = 0;
+long lastFlush = 0;
+byte minSat = 0;
+long latitude;
+long longitude;
+long altitude;
+int PDOP, northVel, eastVel, downVel;
+byte SIV;
+bool isGPSAvailable = 0;
+
+//IMU Setup
+float quats_f[4],rates_f[3],acc_f[3], mag_f[3], temp_f, pres_f;
+byte in[120];
+union {unsigned short s; byte b[2];} checksum;
+union {float f; char c[4]; int32_t i; uint32_t u;} tmp;
+union {double d; char c[8];} tmp_8;
+char dataMsg[142];
+bool isIMUAvailable = 0;
+int bad_imu_chksum = 0;
+
+//blackbox setup
+//char dataString[150];
+String name;
+File myfile;
+
+void setup() {
+
+
+  Serial.begin(115200);
+  delay(100);
+  Serial.println("Serial coms init");
+
+  Serial.print("Initializing SBUS.....");
+  x8r.begin();
+  delay(10);
+  Serial.println("X");
+
+
+  Serial.print("Initializing Lidar....");
+  Wire.begin();
+  sensor.setTimeout(500);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1) {}
+  }
+  sensor.startContinuous();
+  Serial.println("x");
+
+
+  //Serial.print("Initializing blackbox...");
+  //Serial8.begin(115200);
+  //delay(100);
+  //while(!Serial8) {};
+  Serial.println("X");
+  
+  Serial.println("Initializing Sonar....X");
+
+  Serial.print("Initializing GPS....");
+  do {
+    //Serial.println("GPS: trying 115200 baud");
+    Serial5.begin(115200);
+    if (myGPS.begin(Serial5) == true) {
+        //Serial.println("GPS: connected at 115200 baud!");
+        delay(100);
+        break;
+    } else {
+        //myGPS.factoryReset();
+        delay(2000); //Wait a bit before trying again to limit the Serial output
+    }
+
+    //Serial.println("GPS: trying 38400 baud");
+    Serial5.begin(38400);
+    if (myGPS.begin(Serial5) == true) 
+    {
+        //Serial.println("GPS: connected at 38400 baud, switching to 115200");
+        myGPS.setSerialRate(115200);
+        delay(100);
+        break;
+    }
+
+    delay(100);
+    //Serial.println("GPS: trying 9600 baud");
+    Serial5.begin(9600);
+    if (myGPS.begin(Serial5) == true) {
+        //Serial.println("GPS: connected at 9600 baud, switching to 115200");
+        myGPS.setSerialRate(115200);
+        delay(100);
+    }
+
+    delay(100);
+    
+  } while(1);
+
+  myGPS.setUART1Output(COM_TYPE_UBX); //Set the UART port to output UBX only
+  myGPS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
+  myGPS.saveConfiguration(); //Save the current settings to flash and BBR
+
+  myGPS.setUART1Output(COM_TYPE_UBX); //Set the UART port to output UBX only
+  myGPS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
+
+  myGPS.setNavigationFrequency(19);           //Set output to 10 times a second
+  byte rate = myGPS.getNavigationFrequency(); //Get the update rate of this module
+  myGPS.setAutoPVT(true); //Tell the GPS to "send" each solution
+  Serial.print("Current update rate:");
+  Serial.println(rate);
+  
+  myGPS.saveConfiguration(); //Save the current settings to flash and BBR
+  
+  Serial.println("X");
+
+  Serial.println("Searching for satelights");
+
+  while(1) {
+    if(myGPS.getPVT()) {
+      SIV = myGPS.getSIV();
+      if(SIV >= minSat) {
+        break;
+      }
+      else {
+        Serial.print(",");
+        Serial.print(SIV);
+      }
+    }
+  }
+  initIMU();
+  
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+  //if (!SD.sdfs.begin(SdioConfig(DMA_SDIO))) {
+    Serial.println("Card failed, or not present");
+    while (1) {
+      // No SD card, so don't do anything more - stay stuck here
+    }
+  }
+  Serial.println("card initialized.");
+  
+//  String test = SD.sdfs.ls();
+//  Serial.println(test);
+//  Serial.println(test.indexOf("Japan"));
+
+  // increase SD card frequency
+  //bool ok = SD.sdfs.begin(SdSpiConfig(chipSelect, SHARED_SPI, SD_SCK_MHZ(16)));
+
+//  for(int i = 0; i < 100; i++) {
+//    file.getFilename(directory[i]);
+//    if(directory[i] == NULL) {
+//      break;
+//    }
+//  }
+//
+//  for(int i = 0; i < 
+
+
+  delay(1000);
+  threads.setSliceMicros(50);
+  threads.addThread(readSBUS_thread);
+  threads.addThread(readLidar_thread);
+  threads.addThread(readSonar_thread);
+  threads.addThread(readGPS_thread);
+  threads.addThread(readIMU_thread);
+  Serial.println("attaching threads");
+
+
+
+  
+  
+
+  
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //Threads::Mutex mylock;
+  //mylock.lock();
+  //these will go into threads 
+//  readSBUS();
+//  isSbusAvailable = 0;
+//  readLidar();
+//  isLidarAvailable = 0;
+//  readSonar();
+//  isSonarAvailable = 0;
+//  readGPS();
+//  isGPSAvailable = 0;
+
+  // write to SD card if somthing has changed
+  //delay(100);
+
+  // quad has just been armed, open sd log
+//  if(!armed) {
+  if(!armed && channels[4] > 1100 && channels[4] < 2000 && millis() - lastArmed > 500) {
+     
+     // check for log name
+
+      //SD.begin(chipSelect);
+      for(int i = 0; i < 512; i++) {
+        name = "log"+String(i)+".txt";
+        if(!SD.exists(name.c_str())) {
+          break;
+        }
+      }
+
+      
+     //bool ok = SD.sdfs.begin(SdSpiConfig(chipSelect, SHARED_SPI, SD_SCK_MHZ(16)));
+     //SD.sdfs.begin(SdioConfig(DMA_SDIO));
+     //delay(50);
+
+     
+     armed = 1;
+     //FsFile myfile = SD.sdfs.open(name, O_WRITE | O_CREAT);
+     myfile = SD.open(name.c_str(), FILE_WRITE);
+     //unsigned int len = myfile.fileSize();
+     Serial.print(name);
+     Serial.print(" started with ");
+     //Serial.print(len);
+     Serial.println(" bytes");
+//     if (len > 0) {
+//       // reduce the file to zero if it already had data
+//       myfile.truncate();
+//     }
+
+//     if (myfile.preAllocate(106*1000*15*60)) {
+//       Serial.print("  Allocate" +String(106*1000*15*60)+" ");
+//     } else {
+//       Serial.print("  unable to preallocate this file");
+//     }
+
+
+     lastArmed = millis();
+     lastTime = millis();
+     lastFlush = millis();
+//     myfile.println("is anyone there?");
+//     myfile.close();
+//
+//     while(1) {};
+
+  }
+
+  if(millis() - lastFlush > 500 && armed) {
+    myfile.flush();
+    lastFlush = millis();
+    //Serial.println("flushing");
+  }
+
+  if (armed && channels[4] < 1000 && millis() - lastArmed > 500) {
+    armed = 0;
+    lastArmed = millis();
+    //close SD card
+    Serial.println("closing SD");
+    Serial.println(channels[4]);
+    myfile.close();
+  }
+  
+  if((isSbusAvailable || isLidarAvailable || isSonarAvailable || isGPSAvailable || isIMUAvailable) && armed) {
+//    String dataString = "";
+//    dataString += micros(); dataString += ",";
+//    dataString += armed; dataString += ",";
+//  
+//    dataString += isSbusAvailable; dataString += ",";
+//    dataString += channels[0]; dataString += ",";
+//    dataString += channels[1]; dataString += ",";
+//    dataString += channels[2]; dataString += ",";
+//    dataString += channels[3]; dataString += ",";
+//    dataString += channels[4]; dataString += ",";
+//    dataString += channels[5]; dataString += ",";
+//    dataString += channels[6]; dataString += ",";
+//    dataString += channels[7]; dataString += ",";
+//
+//    dataString += isLidarAvailable; dataString += ",";
+//    dataString += String(range, 7);
+//    dataString += ",";
+//
+//    dataString += isSonarAvailable; dataString += ",";
+//    dataString += String(sonarValue, 7);
+//    dataString += ",";
+//
+//    dataString += isGPSAvailable; dataString += ",";
+//    dataString += String(latitude, 15);
+//    dataString += ",";
+//    dataString += String(longitude, 15);
+//    dataString += ",";
+//    dataString += String(altitude, 15);
+//    dataString += ",";
+//    dataString += SIV; dataString += ",";
+//
+//    dataString += isIMUAvailable; dataString += ",";
+//    for (int i = 0; i < 4; i++) 
+//    {
+//      dataString += String(quats_f[i], 7);
+//      dataString += ",";
+//    }
+//    for (int i = 0; i < 3; i++) 
+//    {
+//      dataString += String(rates_f[i], 7);
+//      dataString += ",";
+//    }
+//    for (int i = 0; i < 3; i++) 
+//    {
+//      dataString += String(acc_f[i], 7);
+//      dataString += ",";
+//    }
+//    for (int i = 0; i < 3; i++) 
+//    {
+//      dataString += String(mag_f[i], 7);
+//      dataString += ",";
+//    }
+//    dataString += String(temp_f, 7);
+//    dataString += ",";
+//    dataString += String(pres_f, 7);
+//    
+//    Serial.println(dataString);
+    
+    int idx = 0;
+    dataMsg[idx] = 49; idx ++;
+    dataMsg[idx] = 50; idx++;
+
+    Threads::Mutex mylock;
+    mylock.lock();
+    
+    dataMsg[idx] = isSbusAvailable; idx++;
+    for (int i = 0; i < 8; i++) {
+      tmp.i = channels[i];
+      for (int j = 0; j < 4; j++) {
+        dataMsg[idx] = tmp.c[j];
+        idx++;
+      }
+    }
+    dataMsg[idx] = isLidarAvailable; idx++;
+    tmp.i = range;
+    for (int j = 0; j < 4; j++) {
+        dataMsg[idx] = tmp.c[j];
+        idx++;
+    }
+    dataMsg[idx] = isSonarAvailable; idx++;
+    tmp.i = sonarValue;
+    for (int j = 0; j < 4; j++) {
+        dataMsg[idx] = tmp.c[j];
+        idx++;
+    }
+    dataMsg[idx] = isGPSAvailable; idx++;
+    tmp.i = latitude;
+    for (int j = 0; j < 4; j++) {
+      dataMsg[idx] = tmp.c[j];
+      idx++;
+    }
+    tmp.i = longitude;
+    for (int j = 0; j < 4; j++) {
+      dataMsg[idx] = tmp.c[j];
+      idx++;
+    }
+    tmp.i = altitude;
+    for (int j = 0; j < 4; j++) {
+      dataMsg[idx] = tmp.c[j];
+      idx++;
+    }
+
+    tmp.i = northVel;
+    for (int j = 0; j < 4; j++) {
+      dataMsg[idx] = tmp.c[j];
+      idx++;
+    }
+
+    tmp.i = eastVel;
+    for (int j = 0; j < 4; j++) {
+      dataMsg[idx] = tmp.c[j];
+      idx++;
+    }
+
+    tmp.i = downVel;
+    for (int j = 0; j < 4; j++) {
+      dataMsg[idx] = tmp.c[j];
+      idx++;
+    }
+
+    tmp.i = PDOP;
+    for (int j = 0; j < 4; j++) {
+      dataMsg[idx] = tmp.c[j];
+      idx++;
+    }
+    
+    dataMsg[idx] = SIV; idx++;
+
+    dataMsg[idx] = isIMUAvailable; idx++;
+    for (int i = 0; i < 4; i++) {
+      tmp.f = quats_f[i];
+      for (int j = 0; j < 4; j++) {
+        dataMsg[idx] = tmp.c[j];
+        idx++;
+      }
+    }
+    for (int i = 0; i < 3; i++) {
+      tmp.f = rates_f[i];
+      for (int j = 0; j < 4; j++) {
+        dataMsg[idx] = tmp.c[j];
+        idx++;
+      }
+    }
+    for (int i = 0; i < 3; i++) {
+      tmp.f = acc_f[i];
+      for (int j = 0; j < 4; j++) {
+        dataMsg[idx] = tmp.c[j];
+        idx++;
+      }
+    }
+    tmp.i = micros();
+    for (int j = 0; j < 4; j++) {
+      dataMsg[idx] = tmp.c[j];
+      idx++;
+    }
+    for (int i = 0; i < 3; i++) {
+      tmp.f = mag_f[i];
+      for (int j = 0; j < 4; j++) {
+        dataMsg[idx] = tmp.c[j];
+        idx++;
+      }
+    }
+
+    tmp.f = temp_f;
+      for (int j = 0; j < 4; j++) {
+        dataMsg[idx] = tmp.c[j];
+        idx++;
+      }
+    tmp.f = pres_f;
+      for (int j = 0; j < 4; j++) {
+        dataMsg[idx] = tmp.c[j];
+        idx++;
+      }
+    
+    mylock.unlock();
+    dataMsg[idx] = 60; idx++;
+    dataMsg[idx] = 59; idx++;
+    
+    isSbusAvailable = 0;
+    isLidarAvailable = 0;
+    isSonarAvailable = 0;
+    isGPSAvailable = 0;
+    isIMUAvailable = 0;
+
+    lastTime = millis();
+
+    
+    //myfile.print("test csdfasdfasdfasdfasdfasd");
+    //Serial.println(millis()-lastTime);
+    //Threads::Mutex mylock;
+    //mylock.lock();
+    //Serial.println("writing to file");
+
+    if(myfile) {
+      myfile.write(dataMsg,142);
+
+    //myfile.println("why is this not working?");
+    }
+    else {
+      Serial.println("failed to write");
+    }
+//    Serial.println(millis() - lastTime);
+//    //mylock.unlock();
+//    Serial.println(dataString);
+  }
+  //mylock.unlock();
+//  speedtest = micros();
+  threads.yield();
+
+//  Serial.print(micros()-lastTime);
+//  Serial.print(",");
+//  Serial.print(channels[0]);
+//  Serial.print(",");
+//  Serial.print(range);
+//  Serial.print(",");
+//  Serial.print(sonarValue);
+//  Serial.print(",");
+//  Serial.println(SIV);
+
+  //delay(100);
+
+}
+
+void readSBUS() {
+  if(x8r.read(&channels[0], &failSafe, &lostFrame)){
+
+    if(lostFrame) {
+      Serial.println("lost frame");
+    }
+    isSbusAvailable = 1;
+    // write the SBUS packet to an SBUS compatible servo
+    //Serial.println(channels[0]);
+    int Ltime = micros();
+    //if (armed)
+    //{
+      x8r.write(channels);
+    //}
+    int timeElapsed = micros() - Ltime;
+//      Serial.println(timeElapsed);
+  }
+}
+
+void readLidar() {
+  if(sensor.available())
+  {
+       range = sensor.readRangeMillimeters();
+       isLidarAvailable = 1;
+  }
+}
+
+void readSonar() {
+  sonarValue = analogRead(A0);
+  isSonarAvailable = 1;
+}
+
+void readGPS() {
+  if (myGPS.getPVT()) {
+    latitude = myGPS.getLatitude();
+    longitude = myGPS.getLongitude();
+    altitude = myGPS.getAltitude();
+    SIV = myGPS.getSIV();
+    PDOP = myGPS.getPDOP();
+    northVel = myGPS.getNedNorthVel();
+    eastVel = myGPS.getNedEastVel();
+    downVel = myGPS.getNedDownVel();
+
+    isGPSAvailable = 1;
+  }
+}
+
+void initIMU()
+{
+  Serial.println("Initializing VN-100 Serial (115200)");
+  Serial1.begin(115200);
+  delay(100);
+  Serial.println("turning off output");
+  Serial1.print("$VNASY,0*XX\r\n");
+  delay(100);
+  Serial.println("resetting");
+  Serial1.print("$VNWRG,06,0*XX\r\n");
+  delay(100);
+  Serial.println("Setting output packet rate");
+  Serial1.print("$VNWRG,75,2,1,01,0530*XX\r\n");
+  delay(100);
+  Serial.println("Setting baud rate");
+  Serial1.print("$VNWRG,05,921600*XX\r\n");
+  delay(100);
+  Serial1.flush();
+  delay(100);
+
+  Serial.println("Initializing VN-100 Serial (921600)");
+  Serial1.begin(921600);
+  delay(100);
+  Serial.println("turning off output");
+  Serial1.print("$VNASY,0*XX\r\n");
+  delay(100);
+  Serial.println("resetting");
+  Serial1.print("$VNWRG,06,0*XX\r\n");
+  delay(100);
+  Serial.println("Setting output packet rate");
+  Serial1.print("$VNWRG,75,2,1,01,0530*XX\r\n");
+  delay(100);
+  Serial.println("starting");
+  Serial1.print("$VNASY,1*XX\r\n");
+  delay(100);
+  while (Serial1.available() > 0) {
+    Serial1.read();
+  }
+  Serial.println("Initialized VN-100 Serial");
+}
+
+void readSBUS_thread()
+{
+  while (1)
+  {
+    Threads::Mutex mylock;
+    mylock.lock();
+    readSBUS();
+    mylock.unlock();
+    threads.yield();
+  }
+}
+
+void readLidar_thread()
+{
+  while (1)
+  {
+    readLidar();
+    threads.delay(20);
+    threads.yield();
+  }
+}
+
+void readSonar_thread()
+{
+  while (1)
+  {
+    readSonar();
+    threads.delay(20);
+    threads.yield();
+  }
+}
+
+void readGPS_thread()
+{
+  while (1)
+  {
+    readGPS();
+    threads.yield();
+  }
+}
+
+void readIMU_thread()
+{
+  while (1)
+  {
+    Threads::Mutex mylock;
+    mylock.lock();
+    readIMU();
+    mylock.unlock();
+    threads.yield();
+  }
+}
+
+unsigned short calculate_imu_crc(byte data[], unsigned int length)
+  {
+    unsigned int i;
+    unsigned short crc = 0;
+    for(i=0; i<length; i++){
+      crc = (byte)(crc >> 8) | (crc << 8);
+      crc ^= data[i];
+      crc ^= (byte)(crc & 0xff) >> 4;
+      crc ^= crc << 12;
+      crc ^= (crc & 0x00ff) << 5;
+    }
+    return crc;
+  }
+
+int check_sync_byte(void)
+  {
+    for (int i = 0; i < 6; i++) {
+      Serial1.readBytes(in, 1);
+      if (in[0] == 0xFA) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+void read_imu_data(void) {
+    Serial1.readBytes(in, 65);
+
+    checksum.b[0] = in[64];
+    checksum.b[1] = in[63];
+
+    // uint8_t euler[12]; rpy
+    uint8_t quat[16];
+    uint8_t acc[12];
+    uint8_t rates[12];
+    uint8_t mag[12];
+    uint8_t temp[4];
+    uint8_t pres[4];
+
+    if (calculate_imu_crc(in, 63) == checksum.s) {
+      for (int i = 0; i < 4; i++) {
+        quat[i] = in[3 + i];
+        quat[4+i] = in[7 + i];
+        quat[8+i] = in[11 + i];
+        quat[12+i] = in[15 + i];
+        rates[i] = in[19 + i];
+        rates[4+i] = in[23 + i];
+        rates[8+i] = in[27 + i];
+        acc[i] = in[31 + i];
+        acc[4+i] = in[35 + i];
+        acc[8+i] = in[39 + i];
+        mag[i] = in[43 + i];
+        mag[i+4] = in[47 + i];
+        mag[i+8] = in[51 + i];
+        temp[i] = in[55 + i];
+        pres[i] = in[59 + i];
+      }
+      isIMUAvailable = 1;
+    }
+    else {
+      bad_imu_chksum++;
+      Serial.print("bad imu chksums: "); Serial.println(bad_imu_chksum);
+      return;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+      for (int j = 0; j < 4; j++)
+      {
+        tmp.c[j] = quat[j+i*4];
+      }
+      quats_f[i] = tmp.f;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 4; j++)
+      {
+        tmp.c[j] = rates[j+i*4];
+      }
+      rates_f[i] = tmp.f;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 4; j++)
+      {
+        tmp.c[j] = acc[j+i*4];
+      }
+      acc_f[i] = tmp.f;
+      
+    }
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 4; j++)
+      {
+        tmp.c[j] = mag[j+i*4];
+      }
+      mag_f[i] = tmp.f; 
+    }
+
+    for (int j = 0; j < 4; j++)
+      {
+        tmp.c[j] = temp[j];
+      }
+      temp_f = tmp.f; 
+
+    for (int j = 0; j < 4; j++)
+      {
+        tmp.c[j] = pres[j];
+      }
+      pres_f = tmp.f; 
+    
+  }
+
+int readIMU(void)
+{
+  int check = 0;
+  if (Serial1.available() > 4) {
+    check = check_sync_byte();
+  }
+  if (check == 1) {
+    read_imu_data();
+    
+    return 1;
+  }
+  return 0;
+}
